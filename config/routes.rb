1a7f15d6431adf237e4e68941a6ba54a67764183
Anthony D'Alesandro <ajdalesandro0115@gmail.com>
2023-07-08 15:27:47 -0400

    adding user with controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   get 'portfolio', to: 'portfolio#index'
   get 'home', to: 'home#index'
   root to: 'home#index'
