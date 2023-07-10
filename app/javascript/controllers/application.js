156a324333ffbff999e3578733d8fe402303b310
Tonyy456 <ajdalesandro0115@gmail.com>
2023-07-10 19:26:26 -0400

    unrender form part if box checked

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,23 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+document.addEventListener('DOMContentLoaded', function() {
+    var useLinkCheckbox = document.getElementById('use_link_checkbox');
+    if(useLinkCheckbox == null) return;
+    var form = document.getElementById('rsvp'); // Replace 'your_form_id' with the actual form ID
+    var turnoff = document.getElementById('turn_off_form_checkbox'); // Replace 'your_form_id' with the actual form ID
+  
+    function toggleFormFields() {
+      if(turnoff == null) return;  
+      if (useLinkCheckbox.checked) {
+        turnoff.style.display='none'
+      } else {
+        turnoff.style.display='block'
+      }
+    }
+  
+    useLinkCheckbox.addEventListener('change', toggleFormFields);
+    toggleFormFields(); // Call the function initially to set the initial state of the form fields
+});
+
 export { application }
