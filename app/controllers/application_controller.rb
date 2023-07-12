9950cfbe3c1bed36f822f90875e3cd28dd63f082
Tonyy456 <ajdalesandro0115@gmail.com>
2023-07-12 19:01:59 -0400

    try cleaning up broken blobs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
     before_action :set_url_states
+    before_action :clean_up_blobs
 
     def set_url_states
         session[:previous_request_url] = session[:current_request_url]
         session[:current_request_url] = request.url
+    end
+
+    def clean_up_blobs
+        ActiveStorage::Blob.unattached.where("active_storage_blobs.created_at > ?", 10.seconds.ago).find_each(&:purge_later)
     end
 
     def admin_only
