610adb23e7c54c25f7073ea0799ff9e0f40e767d
Tonyy456 <ajdalesandro0115@gmail.com>
2023-07-21 19:14:02 -0400

    fix

--- app/controllers/strava_controller.rb
+++ app/controllers/strava_controller.rb
@@ -113,9 +113,6 @@ class StravaController < ApplicationController
         begin 
             response = client.oauth_token(grant_type: 'authorization_code', code: params[:code])
         rescue => e
-            e.message # => Bad Request
-            e.errors # => [{ 'code' => 'invalid', 'field' => 'code', 'resource' => 'RequestToken' }]
-            e.headers # => { "status" => "403 Bad Request", "x-ratelimit-limit" => "600,30000", "x-ratelimit-usage" => "314,27536" }
             redirect_to plain: e and return
         end
         set_tokens(response)
