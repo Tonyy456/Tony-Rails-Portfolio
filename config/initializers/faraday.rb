3357f0a88ec0c84860834ed8f7bd56a8d0c9bc55
Tonyy456 <ajdalesandro0115@gmail.com>
2023-07-22 13:59:51 -0400

    fix pt 2

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -2,6 +2,7 @@
 Faraday.new(url: 'https://api.example.com') do |faraday|
     faraday.ssl.verify = true
     faraday.ssl.ca_file = Rails.root.join('lib/ca-certificates.crt').to_s
+    faraday.ssl.ca_path = Rails.root.join('lib/ca-certificates.crt').to_s
     faraday.request :url_encoded
     faraday.adapter Faraday.default_adapter
   end
