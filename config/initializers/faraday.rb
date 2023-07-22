a8ec751ec9c6a6141058a1b18597d5fd7f61e8dd
Tonyy456 <ajdalesandro0115@gmail.com>
2023-07-22 14:04:22 -0400

    ca path env var

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,8 +1,8 @@
 # config/initializers/faraday.rb
 Faraday.new(url: 'https://api.example.com') do |faraday|
     faraday.ssl.verify = true
-    faraday.ssl.ca_file = Rails.root.join('lib/ca-certificates.crt').to_s
-    faraday.ssl.ca_path = Rails.root.join('lib/ca-certificates.crt').to_s
+    faraday.ssl.ca_file = ENV('CA_PATH')
+    faraday.ssl.ca_path = ENV('CA_PATH')
     faraday.request :url_encoded
     faraday.adapter Faraday.default_adapter
   end
