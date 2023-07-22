8187489e2affba00ddb9900cdabee21c02e1b399
Tonyy456 <ajdalesandro0115@gmail.com>
2023-07-22 14:12:08 -0400

    fix notation

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,8 +1,9 @@
 # config/initializers/faraday.rb
-Faraday.new(url: ENV('FARADAY_URL')) do |faraday|
+
+Faraday.new(url: ENV['FARADAY_URL']) do |faraday|
     faraday.ssl.verify = true
-    faraday.ssl.ca_file = ENV('CA_PATH')
-    faraday.ssl.ca_path = ENV('CA_PATH')
+    faraday.ssl.ca_file = ENV['CA_PATH']
+    faraday.ssl.ca_path = ENV['CA_PATH']
     faraday.request :url_encoded
     faraday.adapter Faraday.default_adapter
   end
