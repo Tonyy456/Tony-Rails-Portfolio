ed966626329902ae1d4dd5b2a01f64c268260b1d
Tonyy456 <ajdalesandro0115@gmail.com>
2023-09-04 10:57:30 -0400

    only one sort allowed

--- app/helpers/portfolio_helper.rb
+++ app/helpers/portfolio_helper.rb
@@ -2,22 +2,11 @@ module PortfolioHelper
     def is_a_param(key, value)
         if params.include?(key)
             current = params[key]
-            if current.is_a?(Array)
-                if current.include?(value)
-                    return true
-                else
-                    return false
-                end
-            else
-                if current == value
-                    return true
-                else
-                    return false
-                end 
+            if (current.is_a?(Array) && current.include?(value)) || current == value
+                return true
             end
-        else
-            return false
         end
+        false
     end
 
     def portfolio_params
@@ -26,16 +15,31 @@ module PortfolioHelper
 
     def hash_path(hash, remove = false)
         current_params = portfolio_params.merge(request.query_parameters)
-        #final_params = remove ? remove_parts(current_params, hash) : combine_or_remove_dup(current_params, hash)
-        final_params = new_combine(current_params, hash)
+        final_params = remove ? combine_keep_one(current_params, hash) : combine(current_params, hash)
         portfolio_path(final_params)
     end
 
     private
-    def new_combine(hash1, hash2)
+    def combine_keep_one(hash1, hash2)
         combined_hash = hash1.dup
         hash2.each do |key, value|
-            puts value
+            if combined_hash.key?(key) # extend to array
+                existing_value = combined_hash[key]
+                if existing_value == value
+                    combined_hash.delete(key)
+                else
+                    combined_hash[key] = value
+                end
+            else
+                combined_hash[key] = value
+            end
+        end
+        combined_hash
+    end
+    
+    def combine(hash1, hash2)
+        combined_hash = hash1.dup
+        hash2.each do |key, value|
             if combined_hash.key?(key) # extend to array
                 existing_value = combined_hash[key]
                 if existing_value.is_a?(Array)
